6eeaa82eb0ca5949735d44c996ac140ffc56212f
Zwng <hbanduong@gmail.com>
2024-06-04 07:56:51 +0700

    create activity department, activity employee

--- KTGK/app/src/main/java/com/zwng/ktgk/MainActivity.java
+++ KTGK/app/src/main/java/com/zwng/ktgk/MainActivity.java
@@ -1,6 +1,9 @@
 package com.zwng.ktgk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnDepartment, btnEmployee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnDepartment = findViewById(R.id.btnDepartment);
+        btnEmployee = findViewById(R.id.btnEmployee);
+
+        btnDepartment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DepartmentActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEmployee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EmployeeActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
